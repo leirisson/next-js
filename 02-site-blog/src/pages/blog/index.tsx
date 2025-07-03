690c1faa5ec8c687d5c2f2efb149c12931ce0e00
leirisson <leirissonsouza99@gmail.com>
2025-07-03 14:11:33 -0400

    projetos next

---
+++ 02-site-blog/src/pages/blog/index.tsx
@@ -0,0 +1,7 @@
+export default function Blog() {
+    return(
+        <div>
+            <h2>Blog da seinf</h2>
+        </div>
+    )
+}
---
+++ 02-site-blog/src/pages/blog/user/index.tsx
@@ -0,0 +1,9 @@
+
+
+export default function User() {
+    return (
+        <>
+            <h1>pagina filha usuario.</h1>
+        </>
+    )
+}
