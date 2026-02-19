fdf1c39b0cd37c267b7e34d5261df2cedae722d8
leirisson <103783610+leirisson@users.noreply.github.com>
2026-02-19 17:37:37 -0400

    feat: update firebase configuration to use environment variables

--- 01-tarefas/src/services/firebase.ts
+++ 01-tarefas/src/services/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDyY3l3TrD1vQ_t1-UCi1ckdUyEZ7Y8Mt0",
-    authDomain: "tasks-plus-df293.firebaseapp.com",
-    projectId: "tasks-plus-df293",
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId:  process.env.PROJECT_ID,
     storageBucket: "tasks-plus-df293.firebasestorage.app",
-    messagingSenderId: "228321198494",
-    appId: "1:228321198494:web:8cfdc9157fd7409f94968f"
+    messagingSenderId:  process.env.APP_IDMESSAGING_SENDER_ID,
+    appId:  process.env.APP_ID
 };
 
 
