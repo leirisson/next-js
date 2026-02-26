1444030491843df6711294d9530de77a77b67dd5
leirisson <103783610+leirisson@users.noreply.github.com>
2026-02-26 16:09:36 -0400

    Refactor code structure for improved readability and maintainability

--- 02-smart-list/next.config.ts
+++ 02-smart-list/next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
+  reactStrictMode: false,
+  allowedDevOrigins: ["*"]
+
 };
 
 export default nextConfig;
